5361038145e193a031ce2f07ecf9723bad6d75f5
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-21 17:21:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,14 @@ class Show < ActiveRecord::Base
   end 
   
   def most_popular_show 
-    self.where("rating = ? ", self.)
+    self.where("rating = ? ", self.highest_rating)first 
+    
+  end 
+  
+  def lowest_rating 
+    self.minimum(:rating)
+    
+  end 
   
   
 end 
